b45e4bc6e477eab8e900327f21c3bff5944f10e5
Gagan Prajapati <142085091+Gagan3036@users.noreply.github.com>
2024-07-24 14:53:08 +0530

    exp day 1

---
+++ factorial.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class factorial
+{
+public static void main (String args[])
+{
+    int i, num, fact=1;
+
+    Scanner s = new Scanner(System.in);
+
+    System.out.println("Enter number: ");
+
+    num = s.nextInt(); 
+
+    for(i=1; i<=num; i++)
+    {
+        fact = fact * i;
+    }
+    System.out.println("Factorial is "+fact);
+
+    s.close();
+}
+}
